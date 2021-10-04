bb64fc6ef55c37657f86c4ea04688acbf58341eb
Andrei Kamarouski <komarovsky.andrey.prog@gmail.com>
2021-10-05 00:33:09 +0300

    refactor Subject

--- src/com/solvd/askomar/university/Subject.java
+++ src/com/solvd/askomar/university/Subject.java
@@ -1,6 +1,8 @@
 package com.solvd.askomar.university;
 
-// transl. Предмет аттестата
+import java.util.Objects;
+
+// transl. Предмет ЦТ
 public class Subject {
 
   private String name;
@@ -15,5 +17,25 @@ public class Subject {
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  // TODO refactor equals algorithm
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Subject subject = (Subject) o;
+    return Objects.equals(name, subject.name);
+  }
+
+  // TODO refactor hashcode algorithm
+  @Override
+  public int hashCode() {
+    return Objects.hash(name);
+  }
+
+  @Override
+  public String toString() {
+    return String.format("Subject: %s", this.name);
   }
 }

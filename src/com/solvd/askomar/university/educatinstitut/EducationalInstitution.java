9defe996e651d53239de866a67c781fb52f7d3e4
Andrei Kamarouski <komarovsky.andrey.prog@gmail.com>
2021-10-05 00:07:50 +0300

    feat: add EducationalInstitution

---
+++ src/com/solvd/askomar/university/educatinstitut/EducationalInstitution.java
@@ -0,0 +1,46 @@
+package com.solvd.askomar.university.educatinstitut;
+
+import com.solvd.askomar.university.City;
+
+import java.util.Date;
+
+public abstract class EducationalInstitution {
+
+  private String name;
+  private Date basedDate;
+  private City city;
+
+  public EducationalInstitution(String name) {
+    this.name = name;
+  }
+
+  public EducationalInstitution(String name, Date basedDate, City city) {
+    this.name = name;
+    this.basedDate = basedDate;
+    this.city = city;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Date getBasedDate() {
+    return basedDate;
+  }
+
+  public void setBasedDate(Date basedDate) {
+    this.basedDate = basedDate;
+  }
+
+  public City getCity() {
+    return city;
+  }
+
+  public void setCity(City city) {
+    this.city = city;
+  }
+}

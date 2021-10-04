d147a6637dfe5ca90db64b9b5da2ca281e55d18d
Andrei Kamarouski <komarovsky.andrey.prog@gmail.com>
2021-10-05 00:27:42 +0300

    refactor Specialization

--- src/com/solvd/askomar/university/Specialization.java
+++ src/com/solvd/askomar/university/Specialization.java
@@ -1,5 +1,7 @@
 package com.solvd.askomar.university;
 
+import java.util.Objects;
+
 public class Specialization {
 
   private String name;
@@ -14,5 +16,25 @@ public class Specialization {
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  // TODO refactor equals algorithm
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Specialization that = (Specialization) o;
+    return Objects.equals(name, that.name);
+  }
+
+  // TODO refactor hashcode algorithm
+  @Override
+  public int hashCode() {
+    return Objects.hash(name);
+  }
+
+  @Override
+  public String toString() {
+    return String.format("Specialisation: %s", this.name);
   }
 }

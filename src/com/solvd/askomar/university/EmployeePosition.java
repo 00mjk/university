72cbc2fa608c13c4858c16af44e16bc430c2e27b
Andrei Kamarouski <komarovsky.andrey.prog@gmail.com>
2021-10-05 08:25:21 +0300

    refactor: override Object methods in EmployeePosiion

--- src/com/solvd/askomar/university/EmployeePosition.java
+++ src/com/solvd/askomar/university/EmployeePosition.java
@@ -1,5 +1,7 @@
 package com.solvd.askomar.university;
 
+import java.util.Objects;
+
 public class EmployeePosition {
 
   private String name;
@@ -14,5 +16,25 @@ public class EmployeePosition {
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  // TODO refactor equals algorihm
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    EmployeePosition that = (EmployeePosition) o;
+    return Objects.equals(name, that.name);
+  }
+
+  // TODO refactor hashcode algorithm
+  @Override
+  public int hashCode() {
+    return Objects.hash(name);
+  }
+
+  @Override
+  public String toString() {
+    return String.format("Employee position: %s", this.name);
   }
 }

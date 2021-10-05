8569d193e080153bebe2fec4f5b00d9a1c458d03
Andrei Kamarouski <komarovsky.andrey.prog@gmail.com>
2021-10-05 15:20:08 +0300

    refactor: change Specialisation equals and hashcode

--- src/com/solvd/askomar/university/Specialization.java
+++ src/com/solvd/askomar/university/Specialization.java
@@ -1,7 +1,5 @@
 package com.solvd.askomar.university;
 
-import java.util.Objects;
-
 public class Specialization {
 
   private String name;
@@ -18,19 +16,17 @@ public class Specialization {
     this.name = name;
   }
 
-  // TODO refactor equals algorithm
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     Specialization that = (Specialization) o;
-    return Objects.equals(name, that.name);
+    return name.equals(that.name);
   }
 
-  // TODO refactor hashcode algorithm
   @Override
   public int hashCode() {
-    return Objects.hash(name);
+    return name.hashCode();
   }
 
   @Override

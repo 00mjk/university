1f43218f7e432af7bdd902753dfd98ad8eb878f1
Andrei Kamarouski <komarovsky.andrey.prog@gmail.com>
2021-10-05 15:16:01 +0300

    refactor: change Suject equals and hashcode realisation

--- src/com/solvd/askomar/university/Subject.java
+++ src/com/solvd/askomar/university/Subject.java
@@ -1,7 +1,5 @@
 package com.solvd.askomar.university;
 
-import java.util.Objects;
-
 // transl. Предмет ЦТ
 public class Subject {
 
@@ -19,19 +17,17 @@ public class Subject {
     this.name = name;
   }
 
-  // TODO refactor equals algorithm
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     Subject subject = (Subject) o;
-    return Objects.equals(name, subject.name);
+    return this.name.equals(subject.name);
   }
 
-  // TODO refactor hashcode algorithm
   @Override
   public int hashCode() {
-    return Objects.hash(name);
+    return name.hashCode();
   }
 
   @Override
